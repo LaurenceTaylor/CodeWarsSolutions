c96ee36adf156d22bc38f7202b881a3703569425
Laurence <30178403+LaurenceTaylor@users.noreply.github.com>
2017-10-16 20:37:54 +0100

    Update Beginner.js

--- Beginner.js
+++ Beginner.js
@@ -79,3 +79,25 @@ function XO(str) {
   let o = str.match(/o/gi);
   return (x && x.length) === (o && o.length);
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// Vowel Count
+/////////////////////////////////////////////////////////////////////////////////////////
+
+function getCount(str) {
+  var vowelsCount = 0;
+  for (i = 0; i < str.length; i++) {
+    if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' ||
+        str[i] == 'o' || str[i] == 'u') {
+        vowelsCount += 1;
+    }
+  }
+  return vowelsCount;
+}
+
+////////////////////
+// Best practice:
+
+ function getCount(str) {
+  return (str.match(/[aeiou]/ig)||[]).length;
+}
