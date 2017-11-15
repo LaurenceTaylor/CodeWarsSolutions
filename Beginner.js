2539766ef7671b269a8d6fb3ca55459c7ce82797
Laurence <30178403+LaurenceTaylor@users.noreply.github.com>
2017-11-15 22:04:51 +0000

    Update Beginner.js

--- Beginner.js
+++ Beginner.js
@@ -219,3 +219,17 @@ const GetSum = (a, b) => {
       max = Math.max(a, b);
   return (max - min + 1) * (min + max) / 2;
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// Get the Middle Character
+/////////////////////////////////////////////////////////////////////////////////////////
+
+function getMiddle(s) {
+  if (s.length % 2 == 0) {
+    return s[s.length / 2 - 1] + s[s.length / 2];
+  }
+  
+  else {
+    return s[Math.floor(s.length / 2)];
+  }
+}
